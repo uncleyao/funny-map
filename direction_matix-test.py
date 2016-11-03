901efa5d4df28b696bbe17aa43135b4663baa0c0
Yuan Yao <pengpeng9045@gmail.com>
2016-11-03 01:53:10 -0400

    Update matrix.py
    We ask API one by one to reduce large number of elements in matrix

--- direction_matix-test.py
+++ direction_matix-test.py
@@ -3,37 +3,42 @@ import csv
 from datetime import datetime
 
 # for directions
-gmaps = googlemaps.Client(key='Your API key')
-
+##gmaps = googlemaps.Client(key='AIzaSyDbM-SN7CD6Q60g3LjApyZOcfGujJEpDJg')
+gmaps = googlemaps.Client(key='AIzaSyCOVf2O56NW1LAX8xrqXPmZcYr206lUa7I')
 origins = []
-destinations = []
-f = open('place.csv')
+destinations = ['221 Shepard St, Ripon, WI 54971']
+f = open('active.csv')
 csv_f = csv.reader(f)
 for row in csv_f:
     print(row[0])
     origins.append(row[0])
-    destinations.append(row[1])
 
 now = datetime.now()
-matrix = gmaps.distance_matrix(origins, destinations, language="en-AU",
-                               avoid="tolls",
-                               units="imperial",
-                               departure_time=now,
-                               traffic_model="optimistic")
 
 distance_list = []
 time_list = []
 i = 0
+
+print('heheheheeheheheeheh')
 while i < len(origins):
-    distance_list.append(matrix['rows'][i]['elements'][i]['distance']['text'])
-    time_list.append(matrix['rows'][i]['elements'][i]['duration']['text'])
-    print(matrix['rows'][i]['elements'][i]['distance']['text'], matrix['rows'][i]['elements'][i]['duration']['text'])
+    origins_one = []
+    origins_one.append(origins[i])
+    matrix = gmaps.distance_matrix(origins_one, destinations, language="en-AU",
+                                   avoid="tolls",
+                                   units="imperial",
+                                   departure_time=now,
+                                   traffic_model="optimistic")
+    distance_list.append(matrix['rows'][0]['elements'][0]['distance']['text'])
+    time_list.append(matrix['rows'][0]['elements'][0]['duration']['text'])
+    print(matrix['rows'][0]['elements'][0]['distance']['text'], matrix['rows'][0]['elements'][0]['duration']['text'])
     i = i + 1
 
 rows = zip(distance_list,time_list)
 
-resultFile = open("result.csv", 'w',newline='')
+resultFile = open("output7.csv", 'w',newline='')
 wr = csv.writer(resultFile, dialect='excel')
 for row in rows:
     wr.writerow(row)
+
+
 
