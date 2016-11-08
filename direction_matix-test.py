8bcdace2af4a5de69cc139b76c73ce416c961670
Yuan Yao <pengpeng9045@gmail.com>
2016-11-08 18:08:25 -0500

    Update direction_matix-test.py

--- direction_matix-test.py
+++ direction_matix-test.py
@@ -1,44 +1,56 @@
+__author__ = 'yyao'
+
 import googlemaps
 import csv
-from datetime import datetime
+import datetime
+import time
 
 # for directions
-##gmaps = googlemaps.Client(key='AIzaSyDbM-SN7CD6Q60g3LjApyZOcfGujJEpDJg')
-gmaps = googlemaps.Client(key='AIzaSyCOVf2O56NW1LAX8xrqXPmZcYr206lUa7I')
+# gmaps3 = googlemaps.Client(key='AIzaSyA3hGhAAlTvKXjKSXgh4PVHqb6B3S2tipc')
+gmaps3 = googlemaps.Client(key='AIzaSyAZW8NbmWay7hhpUa1LJXTqclkz8zlWrwU')
+#gmaps2 = googlemaps.Client(key='AIzaSyDbM-SN7CD6Q60g3LjApyZOcfGujJEpDJg')
+#gmaps = googlemaps.Client(key='AIzaSyCOVf2O56NW1LAX8xrqXPmZcYr206lUa7I')
 origins = []
 destinations = ['221 Shepard St, Ripon, WI 54971']
-f = open('active.csv')
+f = open('separations.csv')
 csv_f = csv.reader(f)
 for row in csv_f:
-    print(row[0])
+    #print(row[0])
     origins.append(row[0])
 
-now = datetime.now()
+now = datetime.datetime(2016,11,9)
+print(now)
 
 distance_list = []
 time_list = []
 i = 0
 
+resultFile = open("outputactive501-800.csv", 'a',newline='')
+wr = csv.writer(resultFile, dialect='excel')
 print('heheheheeheheheeheh')
 while i < len(origins):
+    filed = []
     origins_one = []
     origins_one.append(origins[i])
-    matrix = gmaps.distance_matrix(origins_one, destinations, language="en-AU",
+    print(origins_one)
+    if i % 2 == 0:
+        matrix = gmaps3.distance_matrix(origins_one, destinations, language="en-AU",
                                    avoid="tolls",
                                    units="imperial",
                                    departure_time=now,
                                    traffic_model="optimistic")
-    distance_list.append(matrix['rows'][0]['elements'][0]['distance']['text'])
-    time_list.append(matrix['rows'][0]['elements'][0]['duration']['text'])
-    print(matrix['rows'][0]['elements'][0]['distance']['text'], matrix['rows'][0]['elements'][0]['duration']['text'])
-    i = i + 1
-
-rows = zip(distance_list,time_list)
-
-resultFile = open("output7.csv", 'w',newline='')
-wr = csv.writer(resultFile, dialect='excel')
-for row in rows:
-    wr.writerow(row)
-
-
-
+        print(matrix)
+        print(i+1,origins[i],matrix['rows'][0]['elements'][0]['distance']['text'], matrix['rows'][0]['elements'][0]['duration']['text'])
+        filed = [origins[i],matrix['rows'][0]['elements'][0]['distance']['text'], matrix['rows'][0]['elements'][0]['duration']['text']]
+        wr.writerow(filed)
+    else:
+        matrix = gmaps3.distance_matrix(origins_one, destinations, language="en-AU",
+                                   avoid="tolls",
+                                   units="imperial",
+                                   departure_time=now,
+                                   traffic_model="optimistic")
+        print(i+1,origins[i],matrix['rows'][0]['elements'][0]['distance']['text'], matrix['rows'][0]['elements'][0]['duration']['text'])
+        filed = [origins[i],matrix['rows'][0]['elements'][0]['distance']['text'], matrix['rows'][0]['elements'][0]['duration']['text']]
+        wr.writerow(filed)
+    time.sleep(10)
+    i = i +1
